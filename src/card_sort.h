6fd8807d5521955e513420abf845a7b6f6a87c34
TomH <huangtao117@gmail.com>
2015-12-03 11:56:46 +0800

    fixed bug

---
+++ src/card_sort.h
@@ -0,0 +1,39 @@
+/*
+*
+* code: huangtao117@gmail.com
+*
+*/
+#ifndef _SORT_CARD_H
+#define _SORT_CARD_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "card.h"
+
+/* bucket item */
+typedef struct cd_bucket_s{
+    int rank;
+    int num_spade;
+    int num_heart;
+    int num_club;
+    int num_diamond;
+    int num_joker;
+}cd_bucket;
+
+/* sort,compare,etc... */
+void cards_sort(card_t* cards, int len);
+//void rank_bucket(hand_t* hand, int x[]);
+void cards_bucket(card_t* cards, int len, cd_bucket x[]);
+int get_bucket_suit(cd_bucket* item);
+int cards_have_rank(int rank, int x[], int size);
+int card_compare(const void* a, const void* b);
+int card_logicvalue(card_t* card);
+int rank2logic(int rank);
+int logic2rank(int logic);
+int card_interval(card_t* card1, card_t* card2);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
