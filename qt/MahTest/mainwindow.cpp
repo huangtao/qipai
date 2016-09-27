728e364b67cccd5b10ceced10727ba6167a1e8ef
huangtao117 <huangtao117@gmail.com>
2016-09-27 14:36:16 +0800

    fixed bug

--- qt/MahTest/mainwindow.cpp
+++ qt/MahTest/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPainter>
+#include <QResizeEvent>
 
 #include "../../src/mjhz.h"
 
@@ -11,6 +12,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QRect rc = geometry();
+    setFixedSize(rc.width(), rc.height());
+    _winH = rc.height();
+    _winW = rc.width();
 
     for (int i = 1; i < 43; ++i) {
         QString str = QString(":/res/mah/mj%1.png").arg(i);
@@ -27,6 +32,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    QSize sz = event->size();
+    _winW = sz.width();
+    _winH = sz.height();
+    int center_x = _winW / 2;
+    _rcWan.setLeft(2);
+    _rcWan.setTop(2);
+    _rcWan.setRight(_rcWan.left() + 9 * (_imgMj[1].width() + 2));
+    _rcWan.setBottom(_rcWan.top() + _imgMj[1].height());
+    _rcSuo.setLeft(2);
+    _rcSuo.setTop(_rcWan.bottom() + 2);
+    _rcSuo.setRight(_rcSuo.left() + 9 * (_imgMj[1].width() + 2));
+    _rcSuo.setBottom(_rcSuo.top() + _imgMj[1].height());
+    _rcTong.setLeft(2);
+    _rcTong.setTop(_rcSuo.bottom() + 2);
+    _rcTong.setRight(_rcTong.left() + 9 * (_imgMj[1].width() + 2));
+    _rcTong.setBottom(_rcTong.top() + _imgMj[1].height());
+    _rcHornor.setLeft(2);
+    _rcHornor.setTop(_rcTong.bottom() + 2);
+    _rcHornor.setRight(_rcHornor.left() + 7 * (_imgMj[1].width() + 2));
+    _rcHornor.setBottom(_rcHornor.top() + _imgMj[1].height());
+}
+
 void MainWindow::paintEvent(QPaintEvent*)
 {
     int i,j;
@@ -40,6 +69,16 @@ void MainWindow::paintEvent(QPaintEvent*)
     x = y = 2;
     n = 1;
     for (i = 0; i < 3; ++i) {
+        if (i == 0) {
+            x = _rcWan.left();
+            y = _rcWan.top();
+        } else if (i == 1) {
+            x = _rcSuo.left();
+            y = _rcSuo.top();
+        } else {
+            x = _rcTong.left();
+            y = _rcTong.top();
+        }
         for (j = 0; j < 9; ++j) {
             painter.drawPixmap(x, y, _imgMj[n]);
             n++;
@@ -48,6 +87,8 @@ void MainWindow::paintEvent(QPaintEvent*)
         x = 2;
         y += _imgMj[1].height() + 2;
     }
+    x = _rcHornor.left();
+    y = _rcHornor.top();
     for (i = 0; i < 7; ++i) {
         painter.drawPixmap(x, y, _imgMj[n]);
         n++;
--- qt/MahTest/mainwindow.h
+++ qt/MahTest/mainwindow.h
@@ -15,10 +15,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
 private:
     Ui::MainWindow *ui;
     QPixmap _imgMj[43]; // 1-42
+
+    int _winH;
+    int _winW;
+    QRect _rcWan;
+    QRect _rcSuo;
+    QRect _rcTong;
+    QRect _rcHornor;
+    QRect _rcPai;
 };
 
 #endif // MAINWINDOW_H
