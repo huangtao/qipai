19d8959364d456903d13bb29df8bba94109934aa
huangtao <huangtao117@gmail.com>
2016-12-15 22:57:45 +0800

    fixed ios build shell

--- build_iosmac.sh
+++ build_iosmac.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 LIBNAME="qipai"
 IOS_SDK_VERSION="10.1"
 
@@ -15,14 +14,18 @@ cd build.ios
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/iOS.cmake -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/ -GXcode ..
 
 # 构建iphone模拟器64bit
-xcodebuild -project qipai.xcodeproj -alltargets -sdk iphonesimulator"${IOS_SDK_VERSION}" -configuration Release
+#xcodebuild -project qipai.xcodeproj -alltargets -sdk iphonesimulator"${IOS_SDK_VERSION}" -configuration Release
+#xcodebuild -project qipai.xcodeproj -alltargets -sdk iphonesimulator -arch i386 -arch x86_64 -configuration Release
+xcodebuild -project qipai.xcodeproj -alltargets -sdk iphonesimulator -arch x86_64 -configuration Release
 
 cd ..
 mkdir -p lib/x86_64
 cp build.ios/lib/Release/lib"${LIBNAME}".a lib/x86_64
 
 # 构建iphone os
-xcodebuild -project build.ios/qipai.xcodeproj -alltargets -sdk iphoneos"${IOS_SDK_VERSION}" -configuration Release
+#xcodebuild -project build.ios/qipai.xcodeproj -alltargets -sdk iphoneos"${IOS_SDK_VERSION}" -configuration Release
+#xcodebuild -project build.ios/qipai.xcodeproj -alltargets -sdk iphoneos -arch armv7 -arch armv7s -configuration Release
+xcodebuild -project build.ios/qipai.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Release
 
 mkdir -p lib/arm64
 cp build.ios/lib/Release/lib"${LIBNAME}".a lib/arm64
@@ -36,7 +39,11 @@ mv lib"${LIBNAME}".a prebuilt/ios
 #rm -rf lib
 #rm -rf build.ios
 
+echo
+echo "=============================="
 echo "finished build ios fat library"
+echo "=============================="
+echo
 
 # Mac版本
 rm -rf build.mac
@@ -51,4 +58,8 @@ mkdir -p prebuilt/mac
 cp build.mac/lib/Release/lib"${LIBNAME}".a prebuilt/mac/lib"${LIBNAME}".a
 #rm -rf build.mac
 
+echo
+echo "=============================="
 echo "finished build mac library"
+echo "=============================="
+echo
